bcc95009cdeacdefa3ad0a31cdbab7749c956c37
Sabrina Costa <sabrinacostapereira@gmail.com>
2023-11-27 16:25:31 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from mainwidget import MainWidget
+from kivy.lang.builder import Builder
+
+
+class MainApp(App):
+    """Classe com o aplicativo."""
+
+    def build(self):
+        """Método que gera o aplicativo com base no widget principal."""
+        self._widget = MainWidget()
+
+        return self._widget
+    
+if __name__ == '__main__':
+    Builder.load_string(open("mainwidget.kv",encoding="utf-8").read(),rulesonly=True)
+    MainApp().run()
---
+++ mainwidget.py
@@ -0,0 +1,5 @@
+from kivy.uix.boxlayout import BoxLayout    
+
+class MainWidget(BoxLayout):
+    """Classe que representa o widget principal."""
+    pass
