5c9a9c4c84b56f08e89122beaab92d9cf3ee4b99
Sabrina Costa <sabrinacostapereira@gmail.com>
2023-11-27 19:37:08 -0300

    mudança

---
+++ popups.py
@@ -0,0 +1,10 @@
+from kivy.uix.popup import Popup
+
+
+class ModbusPopup(Popup):
+    """Classe que gera o popup de configuração do Modbus."""
+    pass
+
+class ScanPopup(Popup):
+    """Classe que gera o popup de configuração do tempo de varredura."""
+    pass
